8c9c9c829e853aa004a24872edd7db4abb3af2f9
Renan Santos Gomes <renanonline8@gmail.com>
2019-06-12 22:54:24 -0300

    feat: Adiciona post no banco de dados

--- src/index.js
+++ src/index.js
@@ -9,12 +9,6 @@ mongoose.connect(process.env.MONGODB_CONN, {
     useNewUrlParser: true,
 });
 
-/**
- * Stopped Aula 2 - 16:19
- */
-
-app.get('/', (req, res) => {
-    return res.send(`Olá ${req.query.name}`);
-});
+app.use(require('./routes'));
 
 app.listen(3333);
