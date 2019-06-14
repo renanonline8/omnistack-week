01c281026e707a03aa52977a11567e8a58544331
Renan Santos Gomes <renanonline8@gmail.com>
2019-06-14 18:54:37 -0300

    feat: adicionado CORS

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
 mongoose.connect(process.env.MONGODB_CONN, {
     useNewUrlParser: true,
 });
+
+app.use(cors());
 
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
