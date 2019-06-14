76bdc67223473ca9be6ac18dbe71ef9d90b04835
Renan Santos Gomes <renanonline8@gmail.com>
2019-06-14 18:44:24 -0300

    feat: Permitido acesso a imagens
    Pasta resized

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
 mongoose.connect(process.env.MONGODB_CONN, {
     useNewUrlParser: true,
 });
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
 app.use(require('./routes'));
 
